4d47fa259c9b6df0e543f563aea05754df21294d
Hjarno1 <shjarno@gmail.com>
2023-05-31 22:44:38 +0200

    fixed and added test

--- AsteroidsEntityFramework/Player/src/test/java/dk/sdu/mmmi/cbse/playersystem/PlayerPluginTest.java
+++ AsteroidsEntityFramework/Player/src/test/java/dk/sdu/mmmi/cbse/playersystem/PlayerPluginTest.java
@@ -1,36 +1,37 @@
-package dk.sdu.mmmi.cbse.enemysystem;
+package dk.sdu.mmmi.cbse.playersystem;
+
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.player.Player;
 import org.junit.jupiter.api.*;
-import dk.sdu.mmmi.cbse.common.enemy.Enemy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class EnemyPluginTest {
+class PlayerPluginTest {
     private static GameData mockedGameData;
     private static World mockedWorld;
-    private static EnemyPlugin enemy;
+    private static PlayerPlugin player;
 
     @BeforeAll
     static void setUp()  { //executes before all tests
         mockedGameData = mock(GameData.class);
         mockedWorld = mock(World.class);
-        enemy = new EnemyPlugin();
+        player = new PlayerPlugin();
     }
 
     @Test
     @Order(1) //executes first
     void addEnemyToWorld() {
-        enemy.start(mockedGameData, mockedWorld);
-        verify(mockedWorld).addEntity(any(Enemy.class));
+        player.start(mockedGameData, mockedWorld);
+        verify(mockedWorld).addEntity(any(Player.class));
     }
 
     @Test
     @Order(2) //executes second
     void removeEnemyFromWorld() {
-        enemy.stop(mockedGameData, mockedWorld);
-        verify(mockedWorld).removeEntity(any(Enemy.class));
+        player.stop(mockedGameData, mockedWorld);
+        verify(mockedWorld).removeEntity(any(Player.class));
     }
 }
